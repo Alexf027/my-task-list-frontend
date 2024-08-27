764b361f6090267e34ef3b0abe461c82532eaa86
alexf027 <alexfb.027@outlook.es>
2024-08-27 02:15:07 -0500

    credentials deploy

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const instancia1 = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://my-task-list-backend.onrender.com',
     withCredentials: true
 });
 
 const instancia2 = axios.create({
-    baseURL: 'http://localhost:5173', 
+    baseURL: 'https://my-task-list-frontend.vercel.app/', 
     withCredentials: true
 });
 
